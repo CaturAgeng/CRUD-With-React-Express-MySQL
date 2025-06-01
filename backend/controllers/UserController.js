91f850f1a82010a66da1a7c5af43a766bca264b7
CaturAgeng <caturageng023@gmail.com>
2025-06-01 22:39:22 +0700

    Setting controller

---
+++ backend/controllers/UserController.js
@@ -0,0 +1,11 @@
+import User from '../models/User.js';
+
+export const getUsers = async (req, res) => {
+    try {
+        const response = await User.findAll();
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+        console.log(error.message);
+    }
+}
