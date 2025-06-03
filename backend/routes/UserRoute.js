93c6dce32a2bc42254c15c1b7ec27c388196df09
CaturAgeng <caturageng023@gmail.com>
2025-06-03 19:47:56 +0700

    Delete parameter ':id' in route '/users'

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -3,6 +3,6 @@ import { getUsers } from '../controllers/UserController.js';
 
 const router = express.Router();
 
-router.get('/users/:id', getUsers);
+router.get('/users', getUsers);
 
 export default router;
