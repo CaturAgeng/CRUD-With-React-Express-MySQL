ebaa1fa617a33850fb3cb9b76ee8088c1c830d13
CaturAgeng <caturageng023@gmail.com>
2025-06-03 20:09:52 +0700

    Add controller userById and createUser

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -9,3 +9,27 @@ export const getUsers = async (req, res) => {
         console.log(error.message);
     }
 }
+
+export const getUserById = async (req, res) => {
+    try {
+        const response = await User.findOne({
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+        console.log(error.message);
+    }
+}
+
+export const createUser = async (req, res) => {
+    try {
+        await User.create(req.body);
+        res.status(201).json({ msg: "User Created Successfully" });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+        console.log(error.message);
+    }
+}
