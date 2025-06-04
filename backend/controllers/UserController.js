dc6e97af2ad6c8c53dcb10ff62841113506a284e
CaturAgeng <caturageng023@gmail.com>
2025-06-04 23:23:52 +0700

    Add controller update and delete

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -33,3 +33,31 @@ export const createUser = async (req, res) => {
         console.log(error.message);
     }
 }
+
+export const updateUser = async (req, res) => {
+    try {
+        await User.update(req.body, {
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json({ msg: "User Updated Successfully" });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+        console.log(error.message);
+    }
+}
+
+export const deleteUser = async (req, res) => {
+    try {
+        await User.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json({ msg: "User Deleted Successfully" });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+        console.log(error.message);
+    }
+}
