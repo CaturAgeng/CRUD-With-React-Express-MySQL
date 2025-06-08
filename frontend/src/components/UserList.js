e115156598c14be8fb44ba92bfde03d1bc2f6092
CaturAgeng <caturageng023@gmail.com>
2025-06-08 19:42:05 +0700

    Add User Interface and fetch API

--- frontend/src/components/UserList.js
+++ frontend/src/components/UserList.js
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'; 
+import axios from 'axios';
 
 const UserList = () => {
+const [users, setUsers] = useState([]);
+
+useEffect(() => {
+  getUsers();
+}, []);
+
+const getUsers = async () => {
+  const response = await axios.get('http://localhost:5000/users');
+  console.log(response.data);
+}
+
   return (
-    <div className="colums mt-5">
+    <div className="colums mt-5 is-centered">
         <div className="colum is-half">
-            <h1>Lorem ipsum dolor sit amet.</h1>
+            <table className='table is-striped is-fullwidth'>
+              <thead>
+                <tr>
+                  <th>No</th>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Password</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+              </tbody>
+            </table>
         </div>
     </div>
   )
